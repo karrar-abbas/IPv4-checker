373befd4bf9fbb90811a669cc71ad12aa65e7f9d
karrar-abbas <karar.abbas.ch@gmail.com>
2025-04-01 21:09:33 +0300

    delete comments and refactor test.kt .code

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,78 +1,24 @@
 fun main(){
-    // region for correct IPs entered
-    testCheckIpFunction(
-        test = "when check correct ip: 192.168.0.1 return",
-        result = checkIp("192.168.0.1"),
-        correctResult = true
-    )
-    testCheckIpFunction(
-        test = "when check correct ip: 127.0.0.1 return",
-        result = checkIp("127.0.0.1"),
-        correctResult = true
-    )
-    testCheckIpFunction(
-        test = "when check correct ip: 8.8.8.8 return",
-        result = checkIp("8.8.8.8"),
-        correctResult = true
-    )
-    testCheckIpFunction(
-        test = "when check correct ip: 37.238.221.40 return",
-        result = checkIp("37.238.221.40"),
-        correctResult = true
-    )
-    testCheckIpFunction(
-        test = "when check correct ip: 1.0.0.0 return",
-        result = checkIp("1.0.0.0"),
-        correctResult = true
-    )
-    testCheckIpFunction(
-        test = "when check correct ip: 255.255.255.255 return",
-        result = checkIp("255.255.255.255"),
-        correctResult = true
-    )
 
+    // region for correct IPs entered
+
+    testCheckIpFunction(test = "when check correct ip: 192.168.0.1", result = checkIp("192.168.0.1"), correctResult = true)
+    testCheckIpFunction(test = "when check correct ip: 127.0.0.1", result = checkIp("127.0.0.1"), correctResult = true)
+    testCheckIpFunction(test = "when check correct ip: 8.8.8.8", result = checkIp("8.8.8.8"), correctResult = true)
+    testCheckIpFunction(test = "when check correct ip: 37.238.221.40", result = checkIp("37.238.221.40"), correctResult = true)
+    testCheckIpFunction(test = "when check correct ip: 1.0.0.0", result = checkIp("1.0.0.0"), correctResult = true)
+    testCheckIpFunction(test = "when check correct ip: 255.255.255.255", result = checkIp("255.255.255.255"), correctResult = true)
 
     // endregion
 
     // region for incorrect IPs entered
 
-    // three segments not four
-    testCheckIpFunction(
-        test = "when check incorrect ip: 192.0.100 return",
-        result = checkIp("192.0.100"),
-        correctResult = false
-    )
-    // have characters
-    testCheckIpFunction(
-        test = "when check incorrect ip: 192.168.0f.100 return",
-        result = checkIp("192.168.0f.100"),
-        correctResult = false
-    )
-
-    // first segment is above 255
-    testCheckIpFunction(
-        test = "when check incorrect ip: 280.168.0.1 return",
-        result = checkIp("280.168.0.1"),
-        correctResult = false
-    )
-    // Leading zeros in forth segment
-    testCheckIpFunction(
-        test = "when check incorrect ip: 192.168.0.001 return",
-        result = checkIp("192.168.0.001"),
-        correctResult = false
-    )
-    // not use dots for segments
-    testCheckIpFunction(
-        test = "when check incorrect ip: 192.168.0,100 return",
-        result = checkIp("192.168.0,100"),
-        correctResult = false
-    )
-    // enter IPv6
-    testCheckIpFunction(
-        test = "when check incorrect ip: 2600:1400:d:5a3::3bd4 return",
-        result = checkIp("2600:1400:d:5a3::3bd4"),
-        correctResult = false
-    )
+    testCheckIpFunction(test = "three segments not four. incorrect ip: 192.0.100", result = checkIp("192.0.100"), correctResult = false)
+    testCheckIpFunction(test = "have characters. incorrect ip: 192.168.0f.100", result = checkIp("192.168.0f.100"), correctResult = false)
+    testCheckIpFunction(test = "first segment is above 255. incorrect ip: 280.168.0.1", result = checkIp("280.168.0.1"), correctResult = false)
+    testCheckIpFunction(test = "Leading zeros in forth segment. incorrect ip: 192.168.0.001", result = checkIp("192.168.0.001"), correctResult = false)
+    testCheckIpFunction(test = "not use dots for segments. incorrect ip: 192.168.0,100", result = checkIp("192.168.0,100"), correctResult = false)
+    testCheckIpFunction(test = "enter IPv6. incorrect ip: 2600:1400:d:5a3::3bd4", result = checkIp("2600:1400:d:5a3::3bd4"), correctResult = false)
 
     // endregion
 }
@@ -80,7 +26,7 @@ fun main(){
 fun testCheckIpFunction(test:String, result:Boolean, correctResult:Boolean){
 
     if (result == correctResult)
-        println("Success - $test $result")
+        println("Success - $test return $result")
     else
-        println("Failed - $test $result")
+        println("Failed - $test return $result")
 }
