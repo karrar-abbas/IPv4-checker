f56c406fa7b03f8e3887f7db5ea36c66371c7969
karrar-abbas <karar.abbas.ch@gmail.com>
2025-04-02 17:25:00 +0300

    implement checkIp function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,17 @@ fun main() {
 
 }
 
-fun checkIp(ip:String) = false
+fun checkIp(ip:String): Boolean {
+        val ipSegments = ip.split(".")
+
+        return ipSegments.size ==4
+                && ipSegments
+                    .all{
+                        it.all{ it.isDigit() }
+                         && it.toInt() in 0..255
+                         && !(it.length >1 && it.startsWith("0"))
+                    }
+}
+
+
 
